e3a9bb794c4157e9bcee3a1590ae6e75cef4053f
wjq050608 <147054208+wjq050608@users.noreply.github.com>
2023-12-15 15:43:29 +0000

    Add files via upload

---
+++ AddTeacher.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+        body {
+          font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .navbar {
+          overflow: hidden;
+          background-color: #333;
+        }
+
+        .navbar a {
+          float: left;
+          font-size: 16px;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+
+        .dropdown {
+          float: left;
+          overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+          font-size: 16px;
+          border: none;
+          outline: none;
+          color: white;
+          padding: 14px 16px;
+          background-color: inherit;
+          font-family: inherit;
+          margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+          background-color: red;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #f9f9f9;
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          z-index: 1;
+        }
+
+        .dropdown-content a {
+          float: none;
+          color: black;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+          text-align: left;
+        }
+
+        .dropdown-content a:hover {
+          background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+          display: block;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="home.php">Home</a>
+            <div class="dropdown">
+                <button class="dropbtn">View
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="ViewStudent.php">Student</a>
+                    <a href="ViewParent.php">Parent</a>
+                    <a href="ViewTeacher.php">Teacher</a>
+                    <a href="ViewClass.php">Class</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Add
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="AddStudent.php">Student</a>
+                    <a href="AddParent.php">Parent</a>
+                    <a href="AddTeacher.php">Teacher</a>
+                    <a href="AddClass.php">Class</a>
+                </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropbtn">Delete
+                  <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                  <a href="delstudent.php">Student</a>
+                  <a href="delParent.php">Parent</a>
+                  <a href="delTeacher.php">Teacher</a>
+                  <a href="delClass.php">Class</a>
+              </div>
+          </div>
+          <div class="dropdown">
+            <button class="dropbtn">Update
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="updstudent.php">Student</a>
+                <a href="updParent.php">Parent</a>
+                <a href="updTeacher.php">Teacher</a>
+                <a href="updClass.php">Class</a>
+            </div>
+            </div>
+        </div>
+        <h1>Add Teacher</h1>
+        <form method="post">
+        <label for="teacherID">Teacher ID</label>
+          <input type="text" name="teacherID"><br>
+          <label for="teacherName">Teacher Name</label>
+          <input type="text" name="teacherName"><br>
+          <label for="teacherphone">Teacher phone</label>
+          <input type="text" name="teacherphone"><br>
+          <label for="teacherEmail">Teacher Email</label>
+          <input type="text" name="teacherEmail"><br>
+          <input type="submit" value="Add" name="submit"><br>
+        </form>
+
+        <?php
+$link = mysqli_connect("localhost", "root", "", "school");
+if ($link === false) {
+  die("Connection failed: ");
+}
+
+if (isset($_POST['submit'])) {
+    $teacherID = $_POST['teacherID'];
+    $teacherName = $_POST['teacherName'];
+    $teacherphone = $_POST['teacherphone'];
+    $teacherEmail = $_POST['teacherEmail'];
+    $sql = "INSERT INTO teacher (tid,tname,tphone,temail) VALUES ('$teacherID','$teacherName','$teacherphone','$teacherEmail')";
+
+if (mysqli_query($link,$sql)) {
+  echo "New record created successfully";
+} else {
+    echo "Error adding record";
+}
+}
+
+?>
+    </body>
+</html>
